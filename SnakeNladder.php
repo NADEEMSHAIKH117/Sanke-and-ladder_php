3feeda542a8db40f881a90b3086c6a44dd831411
NADEEMSHAIKH117 <nadeemshaikh1171998@gmail.com>
2021-12-24 10:50:51 +0530

    UC3 added

--- SnakeNladder.php
+++ SnakeNladder.php
@@ -1,14 +1,40 @@
 <?php
-class GameOperation{
+class GameOperation
+{
 
-     function diceRoll(){
-        $RollDice= rand(1,6);
-        echo "Snake and Ladder game played with single player at start position 0" ."\n";
-        echo "The Dice Roll is: ".$RollDice;
+    function diceRoll()
+    {
+        $RollDice = rand(1, 6);
+        echo "Snake and Ladder game played with single player at start position 0" . "\n";
+        echo "The Dice Roll is: " . $RollDice ."\n";
+    }
 
+    function playerCheck()
+    {
+        $PlayerCheck = rand(0,2);
+        echo "player check: ". $PlayerCheck ."\n";
+
+        /**
+         * PlayerCheck=0 Then Player goes down by snake
+         * PlayerCheck=1 Then Player Not play
+         * PlayerCheck=2 Then Player Climb Ladder
+         */
+        switch ($PlayerCheck) {
+            case 0:
+                echo "SNAKE";
+                break;
+            case 1:
+                echo "NO PLAY";
+                break;
+            case 2:
+                echo "LADDER"."\n";
+                $RollDice = rand(1, 6);
+                echo "The Dice Roll is: " . $RollDice ."\n";
+                break;
+        }
     }
 }
 
 $game = new GameOperation();
 $game->diceRoll();
-?>
+$game->playerCheck();
